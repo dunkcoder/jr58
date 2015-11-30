a7679230ea8866347ed08c09ff4e53c320afc165
孟祥瑞 <429217743@qq.com>
2015-11-30 09:49:32 +0800

    修改

--- jr58.js
+++ jr58.js
@@ -1,4 +1,12 @@
+/*
+ * jello jr58
+ */
+
+ 'use strict';
+
 var fis = module.exports = require('jello');
 
 fis.cli.name = 'jr58';
+
 fis.cli.info = fis.util.readJSON(__dirname + '/package.json');
+
